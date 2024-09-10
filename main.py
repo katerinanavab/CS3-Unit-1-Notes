94fab7824ee31ec1e0ec848cba9699b070c85848
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-10 19:09:44 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     print("hello world")
 
+    # Declare variables
+    age = 17
+    teacher = "me"
+    is_fave_class = True
+    my_grade = 99.99
+
 
 if __name__ == "__main__":
     main()

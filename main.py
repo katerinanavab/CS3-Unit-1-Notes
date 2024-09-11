3358ee6d745433226aee7a352db5de41514d3c22
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-11 14:39:11 +0000

    edit main.py

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ def main():
     is_fave_class = True
     my_grade = 99.99
 
+    # Check the type of a variable
+    type_of_age = type(age)
+    print(type_of_age)
+
+    # String formatting with f-strings 
+    # f"String literal {variable}"
+    print(f"The type of the variable age is: {type_of_age}")
+    print(f"My age divided by 10: {age/10}")
+
+
+
+def bake_cookie(ingredients, instructions, temperature):
+    # Print the list of ingredients
+
+    # Print the oven temperature setting
+
+    # Print the instructions 
+    print(instructions)
+
 
 if __name__ == "__main__":
     main()

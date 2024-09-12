e7c1d6ad18a5daa19c9005583dac71fb6d381687
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-12 18:25:06 +0000

    edit main.py

--- main.py
+++ main.py
@@ -16,15 +16,30 @@ def main():
     print(f"The type of the variable age is: {type_of_age}")
     print(f"My age divided by 10: {age/10}")
 
+    # Initialize args for function
+    ingredients_list = ["chocolate chips", "cinnamon", "flour", "sugar", "butter", "eggs"]
+    snickerdoodle = "mix everything and put in oven. there ya go!"
+    temp = 300
+
+    # Call a function
+    bake_cookie(ingredients_list, snickerdoodle, temp)
+
+    # Call a function with an optional argument
+    bake_cookie(ingredients_list, snickerdoodle, temp, cutter="star")
 
 
-def bake_cookie(ingredients, instructions, temperature):
+def bake_cookie(ingredients, instructions, temperature, cutter="circle"):
+
     # Print the list of ingredients
-
+    for item in ingredients:
+        print(item)
     # Print the oven temperature setting
-
+    print(f"Set the oven to {temperature} degrees Kelvin.")
+    print() # line break
     # Print the instructions 
-    print(instructions)
+    print(instructions, end="\n \n") # use optional keyword arg for line break
+    # Tell them which cookie cutter to use
+    print(f"Now use a {cutter} cookie cutter")
 
 
 if __name__ == "__main__":
